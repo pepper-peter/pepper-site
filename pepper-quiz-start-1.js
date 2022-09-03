0edd79722bec47cea3cc973263eb839c4d32daf9
pepper-peter <112542912+pepper-peter@users.noreply.github.com>
2022-09-03 17:43:42 +0800

    Update pepper-quiz-start-1.js

--- pepper-quiz-start-1.js
+++ pepper-quiz-start-1.js
@@ -33,6 +33,10 @@ $('.email-button').on('click',function(){
         kpd = Math.round((Vgoalweight)**.75*70*Vage*Vspayed*Vactivity*Vtreats/10)*10;
     }
     kpdf = kpd/2;
+    console.log('kpdd = ' + kpd);
+    console.log('kpdf = ' + kpdf);
+    console.log('Vage = ' + Vage);
+    console.log('Vcurrentweight = ' + Vcurrentweight);
     $('.dog_kcal_out').val(kpd);
     $('.dog_kcal_out_half').val(kpdf);
 });
